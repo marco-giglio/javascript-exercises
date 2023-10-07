1ae164a904031fd685764950ffbb778964062d77
Marco <55298054+marco-giglio@users.noreply.github.com>
2023-10-07 20:00:59 +0100

    complete excercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // Year divisible by 4 returns true
+    // Year divisible by 100 return false unless also divisible by 400
+
+    if (((year % 100 === 0) && (year % 400 === 0) ) || ((year % 4 === 0) && (year % 100 != 0))) {
+       return true
+    }
+
+    return false
 };
 
 // Do not edit below this line
