4d19987ff4922419f39adae1e5888a45394fbfe2
Marco <55298054+marco-giglio@users.noreply.github.com>
2023-10-07 18:00:21 +0100

    complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+
+    if (start > end) {
+        [start, end] = [end, start]
+    }
+
+    if (start < 0 || end < 0) {
+        return 'ERROR'
+    }
+
+    if (typeof start != 'number' || typeof end != 'number') {
+        return 'ERROR'
+    }
+
+    const arr = []
+
+    for (let i = start; i <= end; i++){
+        arr.push(i)
+    }
+
+    let sum = 0 
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+
+    return sum
 
 };
 
