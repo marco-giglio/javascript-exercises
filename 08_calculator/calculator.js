ea1bede15362edbbb3e4ff9f4cbfcc66ee3ae845
Marco <55298054+marco-giglio@users.noreply.github.com>
2023-10-10 19:41:48 +0100

    complete Wes Bos JS excercises 2

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(a,b) {
+    return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+
+  return arr.reduce((a,b) => a+b , 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a * b)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  // A factorial is the product of all positive integers less than or equal to n. 
+  // 0! === 1
+
+  if (n === 0 ) {
+    return 1
+  }
+
+  arr = []
+  for (let i = 1; i <= n; i++) {
+    arr.push(i);
+  }
+
+  return arr.reduce((a,b) => a * b);
+
 };
 
 // Do not edit below this line
